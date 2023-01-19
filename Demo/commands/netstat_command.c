0b3260932f35df531a7fc30d213be18ceba063c4
kar-rahul-aws <karahulx@amazon.com>
2023-01-19 23:38:05 +0530

    Netstat changes on the device side

--- Demo/commands/netstat_command.c
+++ Demo/commands/netstat_command.c
@@ -39,19 +39,19 @@ static portBASE_TYPE prvNetStatCommandInterpreter( char *pcWriteBuffer, size_t x
             	if( strncmp( pcCommandParameter, "start", xCommandParameterLength ) == 0 )
             	{
             		xResult = vGetNetStat(eStartStat, &( all_network_stats ));
-            		configPRINTF(("%d\n",xResult));
+            		snprintf( ( char * ) pcWriteBuffer, xWriteBufferLen, "Start Capturing Netstat" );
             	}
             	else if( strncmp( pcCommandParameter, "stop", xCommandParameterLength ) == 0 )
             	{
             		xResult = vGetNetStat(eStopStat, &( all_network_stats ));
-            		configPRINTF(("%d\n",xResult));
+            		snprintf( ( char * ) pcWriteBuffer, xWriteBufferLen, "Stop Capturing Netstat" );
             	}
             	else if( strncmp( pcCommandParameter, "get", xCommandParameterLength ) == 0 )
                 {
 
 					xResult = vGetNetStat(eGetStat, &( all_network_stats ));
-					configPRINTF(("%d\n",xResult));
-					snprintf( pcWriteBuffer, xWriteBufferLen, "%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%lld,%lld,",
+
+					snprintf( pcWriteBuffer, xWriteBufferLen, "%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,",
 																all_network_stats.udp_stat.stat.pckt_rx,
 																all_network_stats.udp_stat.stat.pckt_tx,
 																all_network_stats.udp_stat.stat.pcket_drop_rx,
@@ -64,8 +64,6 @@ static portBASE_TYPE prvNetStatCommandInterpreter( char *pcWriteBuffer, size_t x
 																all_network_stats.tcp_stat.stat.pcket_drop_tx,
 																all_network_stats.tcp_stat.stat.bytes_rx,
 																all_network_stats.tcp_stat.stat.bytes_tx,
-																all_network_stats.tcp_stat.out_ack,
-																all_network_stats.tcp_stat.in_ack,
 																all_network_stats.icmp_stat.stat.pckt_rx,
 																all_network_stats.icmp_stat.stat.pckt_tx,
 																all_network_stats.icmp_stat.stat.pcket_drop_rx,
